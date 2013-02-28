13bd74cd2b64b56e0c038f9d77d91862fb9e0d5c
Tom Klun <tom@tomklun.com>
2013-02-27 21:50:20 -0600

    Stubbing out new files. Adding some serial data hooks

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-  var SerialPort = require("serialport").SerialPort;
-  var serialPort = new SerialPort("/dev/ttyACM0", {
-    baudrate: 57600
-  });
-
-serialPort.on("open", function () {
-  console.log('open');
-  serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
-  });
-  serialPort.write("ls\n", function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-});
+module.exports = process.env.EXPRESS_COV
+  ? require('./lib-cov/brewpi')
+  : require('./lib/app');
