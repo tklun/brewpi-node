90bc0e9aacf0d89abed2fbb937e403d7c36f6731
Tom Klun <tom@tomklun.com>
2013-02-27 21:51:41 -0600

    Adding test debug line

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = process.env.EXPRESS_COV
-  ? require('./lib-cov/brewpi')
-  : require('./lib/app');
+// module.exports = process.env.EXPRESS_COV
+//   ? require('./lib-cov/brewpi')
+//   : require('./lib/app');
+
+var brewPi = require('./lib/app');
+
+brewPi.createBrewPi();
