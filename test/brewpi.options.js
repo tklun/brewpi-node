f24aba0b87fc2ed0cb949fcc98e742d1a1745e3d
Tom Klun <tom@tomklun.com>
2013-03-02 22:33:04 -0600

    Adding first tests

---
+++ test/brewpi.options.js
@@ -0,0 +1,22 @@
+var BrewPi = require('../'),
+    should = require('should');
+
+describe('brewPi', function(){
+  describe('serial port configuration', function(){
+
+    it('should be able to be passed a serial port', function(){
+      var testBrewPi = new BrewPi({
+        serialPort: '/dev/test/port'
+      });
+
+      testBrewPi.serialPort.should.equal('/dev/test/port');
+    });
+
+    it('should have a default serial port', function(){
+      var testBrewPi = new BrewPi({});
+
+      testBrewPi.serialPort.should.equal('/dev/ttyACM0');
+    });
+
+  });
+});
