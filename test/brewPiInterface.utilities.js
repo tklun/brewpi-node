e24a51e706c6fa1e05f17b169d7fe67373f57d1f
Tom Klun <tom@tomklun.com>
2013-03-04 22:17:06 -0600

    Adding interface tests

--- test/brewPiInterface.utilities.js
+++ test/brewPiInterface.utilities.js
@@ -2,26 +2,46 @@ var BrewPi = require('../'),
     should = require('chai').should(),
     sinon = require('sinon'),
     EventEmitter = require('events').EventEmitter,
-    BrewPiInterface = require('../lib/interface'),
-    stripKey = require('../lib/interface').stripKey;
+    csrequire = require('covershot').require.bind(null, require),
+    BrewPiInterface = csrequire('../lib/interface'),
+    stripKey = csrequire('../lib/interface').stripKey;
 
 describe('brewPiInterface', function(){
+  var testBrewPiInterfaceInstance;
+
+  beforeEach(function(){
+    testBrewPiInterfaceInstance = new BrewPiInterface({
+      brewPi: new BrewPi()
+    });
+  });
+
   describe('utilities', function(){
     it('should be able to strip a key from an Arduino response', function(){
+      // var testBrewPiInterfaceInstance = new BrewPiInterface({
+      //   brewPi: new BrewPi()
+      // });
+
       var arduinoString = "T:{\"BeerTemp\": 40",
           strippedString = stripKey(arduinoString);
       strippedString.should.equal("{\"BeerTemp\": 40");
     });
   });
 
-  describe('event emitters', function() {
+  describe('update temperature', function() {
+    it('should emmit an event after the temperature is stored', function() {
+      var spy = sinon.spy();
+
+      testBrewPiInterfaceInstance.brewPi.on('newTemperatureData', spy);
+
+      testBrewPiInterfaceInstance.storeNewTemperatures('T:{"stubbed": "settings"}');
+      spy.called.should.equal(true);
+    });
+  });
+
+  describe('update LCD', function() {
 
     it('should emit an event when LCD is updated', function() {
       var spy = sinon.spy();
-
-      var testBrewPiInterfaceInstance = new BrewPiInterface({
-        brewPi: new BrewPi()
-      });
 
       testBrewPiInterfaceInstance.brewPi.on('newLCDData', spy);
 
@@ -29,9 +49,47 @@ describe('brewPiInterface', function(){
       spy.called.should.equal(true);
     });
 
-
+    it('should do something with the LCD data');
 
   });
 
+  describe('update control constants', function () {
+    it('should update control constants');
+
+    it('should emit an event when update is complete', function() {
+      var spy = sinon.spy();
+
+      testBrewPiInterfaceInstance.brewPi.on('controlConstantsUpdated', spy);
+
+      testBrewPiInterfaceInstance.updateControlConstants('C:{"stubbed": "constants"}');
+      spy.called.should.equal(true);
+    });
+  });
+
+  describe('update control settings', function () {
+    it('should update control settings');
+
+    it('should emit an event when update is complete', function() {
+      var spy = sinon.spy();
+
+      testBrewPiInterfaceInstance.brewPi.on('controlSettingsUpdated', spy);
+
+      testBrewPiInterfaceInstance.updateControlSettings('S:{"stubbed": "settings"}');
+      spy.called.should.equal(true);
+    });
+  });
+
+  describe('update control variables', function () {
+    it('should update control variables');
+
+    it('should emit an event when update is complete', function() {
+      var spy = sinon.spy();
+
+      testBrewPiInterfaceInstance.brewPi.on('controlVariablesUpdated', spy);
+
+      testBrewPiInterfaceInstance.updateControlVariables('V:{"stubbed": "constants"}');
+      spy.called.should.equal(true);
+    });
+  });
 
 });
