1067a06bab51c4bb9632c254f093e9fc95de4ae9
Tom Klun <tom@tomklun.com>
2013-03-05 22:20:16 -0600

    Implementing serial port mock and serial port write tests

--- test/brewPi.js
+++ test/brewPi.js
@@ -1,7 +1,9 @@
 var BrewPi = require('../'),
     should = require('chai').should(),
+    sinon = require('sinon'),
     csrequire = require('covershot').require.bind(null, require),
-    BrewPi = csrequire('../lib/brewpi');
+    BrewPi = csrequire('../lib/brewpi'),
+    SerialPort = require('./MockSerialPort').SerialPort;
 
 describe('brewPi', function(){
   describe('instantiation', function(){
@@ -67,6 +69,83 @@ describe('brewPi', function(){
       var setting = testBrewPiInstance.getControlVariables();
 
       testBrewPiInstance.settings.controlVariables.should.equal(setting);
+    });
+
+  });
+
+  describe('write to arduino through serial port', function () {
+
+    var testBrewPiInstance,
+        spy;
+
+    beforeEach(function(){
+      testBrewPiInstance = new BrewPi();
+      testBrewPiInstance.brewPiConnector = new SerialPort('/path/to/fake/usb');
+      spy = sinon.spy(testBrewPiInstance.brewPiConnector, 'write');
+    });
+
+    afterEach(function() {
+      testBrewPiInstance.brewPiConnector.write.restore();
+    });
+
+    describe('control settings', function() {
+      it('should be write to refresh the control settings once', function () {
+        testBrewPiInstance.refreshControlSettings();
+        spy.calledOnce.should.equal(true);
+      });
+
+      it('should be write \'s\' to the serial port', function(){
+        testBrewPiInstance.refreshControlSettings();
+        spy.calledWith('s').should.equal(true);
+      });
+    });
+
+    describe('control constants', function() {
+      it('should be write to refresh the control constants once', function () {
+        testBrewPiInstance.refreshControlConstants();
+        spy.calledOnce.should.equal(true);
+      });
+
+      it('should be write \'c\' to the serial port', function(){
+        testBrewPiInstance.refreshControlConstants();
+        spy.calledWith('c').should.equal(true);
+      });
+    });
+
+    describe('control variables', function() {
+      it('should be write to refresh the control variables once', function () {
+        testBrewPiInstance.refreshControlVariables();
+        spy.calledOnce.should.equal(true);
+      });
+
+      it('should be write \'v\' to the serial port', function(){
+        testBrewPiInstance.refreshControlVariables();
+        spy.calledWith('v').should.equal(true);
+      });
+    });
+
+    describe('load default arduino settings', function() {
+      it('should be write load default arduino settings', function () {
+        testBrewPiInstance.loadDefaultControlSettings();
+        spy.calledOnce.should.equal(true);
+      });
+
+      it('should be write \'S\' to the serial port', function(){
+        testBrewPiInstance.loadDefaultControlSettings();
+        spy.calledWith('S').should.equal(true);
+      });
+    });
+
+    describe('load default arduino constants', function() {
+      it('should be write load default arduino constants', function () {
+        testBrewPiInstance.loadDefaultControlConstants();
+        spy.calledOnce.should.equal(true);
+      });
+
+      it('should be write \'C\' to the serial port', function(){
+        testBrewPiInstance.loadDefaultControlConstants();
+        spy.calledWith('C').should.equal(true);
+      });
     });
 
   });
