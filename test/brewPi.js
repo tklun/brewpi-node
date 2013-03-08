268c6caeb1751b4dbce812cb5114267f959dcff4
Tom Klun <tom@tomklun.com>
2013-03-07 23:14:05 -0600

    More brewpi tests

--- test/brewPi.js
+++ test/brewPi.js
@@ -18,11 +18,18 @@ describe('brewPi', function(){
 
   describe('create', function() {
 
+    // Not actually being called every tiem
     beforeEach(function(){
       var testBrewPiInstance = new BrewPi();
     });
 
     it('should have a reference to it\'s parent context');
+
+    it('should create a new brewPiConnector and store it as part of itself');
+
+    it('should listen for new data upon connection');
+
+    it('should fire a callback function when connection has been opened');
 
 
   });
